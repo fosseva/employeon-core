d6dff6fe0aa512b76776572231a8f906e7a02954
Aniket Magadum <aniketmagadum77@gmail.com>
2026-07-20 22:10:26 +0530

    Make package more minimal

--- src/EmployeonServiceProvider.php
+++ src/EmployeonServiceProvider.php
@@ -2,14 +2,11 @@
 
 namespace Employeon;
 
-use Employeon\Contracts\EmployeonManager;
 use Illuminate\Support\ServiceProvider;
 
 class EmployeonServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(Employeon::class, fn (): Employeon => new Employeon);
-        $this->app->alias(Employeon::class, EmployeonManager::class);
     }
 }
